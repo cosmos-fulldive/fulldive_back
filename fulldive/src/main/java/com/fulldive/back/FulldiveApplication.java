a5edae4e037b079e8df9e34b055a9229f946c439
Jeonghwan-Choi <yzon007@naver.com>
2022-12-20 15:40:46 +0900

    git commmit test

--- fulldive/src/main/java/com/fulldive/back/FulldiveApplication.java
+++ fulldive/src/main/java/com/fulldive/back/FulldiveApplication.java
@@ -2,7 +2,9 @@ package com.fulldive.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+* 풀다이브 시작
+ */
 @SpringBootApplication
 public class FulldiveApplication {
 
